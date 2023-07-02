d2aeb2a6555c501e90982d7a37d4ab65fede6d6a
Kerley Ribeiro <kerleyribeiro@hotmail.com>
2023-07-02 16:26:26 -0300

    Update script.js
    script para validação de usuários

--- src/script.js
+++ src/script.js
@@ -18,3 +18,64 @@ function CriaPDF() {
     win.document.close(); 	                                         // FECHA A JANELA
     win.print();                                                         
   }
+
+function entrar() {
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('senha').value;
+
+  // Faz a requisição para obter os usuários
+  fetch('https://pco-si-2023-1-p1-proj-web-t1-grupo-1-quitay.vercel.app/usuarios.json')
+    .then(response => response.json())
+    .then(data => {
+      var usuarios = data.usuarios;
+      var usuarioEncontrado = usuarios.find(usuario => usuario.email === email && usuario.senha === senha);
+
+      if (usuarioEncontrado) {
+        alert('Usuário autenticado com sucesso!');
+        // Redireciona para a próxima página após autenticação bem-sucedida
+        window.location.href = 'index.html';
+      } else {
+        alert('Email ou senha inválidos.');
+      }
+    })
+    .catch(error => {
+      console.log('Ocorreu um erro ao realizar a requisição:', error);
+    });
+}
+
+function cadastrar() {
+  var nome = document.getElementById('nomeCadastro').value;
+  var email = document.getElementById('emailCadastro').value;
+  var senha = document.getElementById('senhaCadastro').value;
+  var confirmarSenha = document.getElementById('confirmarSenhaCadastro').value;
+
+  if (senha !== confirmarSenha) {
+    alert('As senhas não coincidem.');
+    return;
+  }
+
+  var usuario = {
+    nome: nome,
+    email: email,
+    senha: senha
+  };
+
+  // Faz a requisição para adicionar um novo usuário
+  fetch('https://pco-si-2023-1-p1-proj-web-t1-grupo-1-quitay.vercel.app/usuarios', {
+    method: 'POST',    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(usuario)
+  })
+    .then(response => response.json())
+    .then(data => {
+      alert('Usuário cadastrado com sucesso!');
+      // Redireciona para a próxima página após cadastro bem-sucedido
+      window.location.href = 'login.html';
+    })
+    .catch(error => {
+      console.log('Ocorreu um erro ao cadastrar o usuário:', error);
+    });
+}
+
+   
